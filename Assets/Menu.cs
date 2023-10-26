5ba00010b6035ad98c4d420617a00140cf13f42c
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-10-27 00:16:04 +0200

    Porjet

---
+++ Assets/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void JouerJeu()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitterJeu()
+    {
+        Application.Quit();
+    }
+
+}
